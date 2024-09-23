0d21017d264b5ecce7b9cd6db58c3c6f4d45ab96
Elvis Henrique Pereira <elvishp2006@gmail.com>
2024-09-22 23:11:42 -0300

    Update run-tailscale.sh

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -3,8 +3,7 @@
 /render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 PID=$!
 
-ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
-until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES" --advertise-exit-node; do
+until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-exit-node; do
   sleep 0.1
 done
 export ALL_PROXY=socks5://localhost:1055/
